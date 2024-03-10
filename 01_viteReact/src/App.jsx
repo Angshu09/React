564d1cf39ee441c9d6fdb9d9f8405a05d3742f73
Angshu Das <angshudas191@gmail.com>
2024-03-10 19:25:59 +0530

    modified

--- 01_viteReact/src/App.jsx
+++ 01_viteReact/src/App.jsx
@@ -1,10 +1,10 @@
 import Chai from './chai.jsx';
 
 function App() {
-
+  const username = "Angshu Das";
   return (
     <>
-    <h1>Chai Aur React with vite | Angshu Das</h1>
+    <h1>Chai Aur React with vite | {username}</h1>
     <Chai />
     <h3>This is a react vite page</h3>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, soluta.</p>
