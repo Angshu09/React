5f5303dc62ed1f819eebe07b249c86cd1e7b19e1
Angshu Das <angshudas191@gmail.com>
2024-03-19 23:34:51 +0530

    added

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import InputBox from './components/Input'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+function App() {
+
+  return (
+    <>
+      <InputBox/>
+    </>
+  )
+}
+
+export default App
