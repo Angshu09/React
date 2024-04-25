599a8391c256e8eec4245e6e446f307350a574ff
Angshu Das <angshudas191@gmail.com>
2024-04-25 23:52:32 +0530

    modified for practice

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react"
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState('olive')
 
   return (
-   <div className="w-full h-screen duration-200" style={{backgroundColor : color}}>
+   <div className="w-full flex justify-center h-screen duration-150 relative" style={{backgroundColor : color}}>
 
-    <div className=" flex flex-wrap justify-center fixed bottom-10 inset-x-0 px-2">
-      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-slate-50 px-3 py-3 rounded-full">
-      <button onClick={() => setColor('green')} className="px-7 py-2 text-cyan-50 rounded-3xl" style={{backgroundColor: "green"}}>green</button>
-      <button onClick={() => setColor('red')} className="px-7 py-2 text-cyan-50 rounded-3xl" style={{backgroundColor: "red"}}>Red</button>
-      <button onClick={() => setColor('yellow')} className="px-7 py-2 text-black rounded-3xl" style={{backgroundColor: "yellow"}}>Yellow</button>
-      <button onClick={() => setColor('brown')} className="px-7 py-2 text-black rounded-3xl" style={{backgroundColor: "brown"}}>Brown</button>
-      <button onClick={() => setColor('aqua')} className="px-7 py-2 text-black rounded-3xl" style={{backgroundColor: "aqua"}}>Aqua</button>
-      <button onClick={() => setColor('pink')} className="px-7 py-2 text-black rounded-3xl" style={{backgroundColor: "pink"}}>Pink</button>
-      </div>
+    <div className="flex rounded-lg items-center gap-2 py-2 px-2 fixed bottom-5 bg-slate-50">
+      
+      <button onClick={() => setColor("red")} className=" bg-red-500 py-2 px-8 rounded-xl text-cyan-50">Red</button>
+      <button onClick={() => setColor("yellow")} className=" bg-yellow-500 py-2 px-8 rounded-xl text-cyan-50">yellow</button>
+      <button onClick={() => setColor("blue")} className=" bg-blue-500 py-2 px-8 rounded-xl text-cyan-50">blue</button>
+      <button onClick={() => setColor("green")} className=" bg-green-500 py-2 px-8 rounded-xl text-cyan-50">green</button>
+      <button onClick={() => setColor("pink")} className=" bg-pink-500 py-2 px-8 rounded-xl text-cyan-50">pink</button>
+
     </div>
 
    </div>
