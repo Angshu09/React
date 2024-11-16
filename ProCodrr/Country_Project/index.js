7dc45c648e9c819b43413b78015b9f8f8b4cbe4f
Angshu Das <angshudas191@gmail.com>
2024-11-16 09:08:54 +0530

    update the country project folder

--- ProCodrr/Country_Project/index.js
+++ ProCodrr/Country_Project/index.js
@@ -2,34 +2,32 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact from "./Components/ContactUs/Contact";
-import Home from "./Components/Home/Home"
+import Home from "./Components/Home/Home";
 import ErrorPage from "./Components/Error/ErrorPage";
 import CountryDetail from "./Components/CountryDetail/CountryDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    errorElement: <ErrorPage/>,
+    element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "/contact",
-        element: <Contact/>
+        element: <Contact />,
       },
       {
         path: "/:country",
-        element: <CountryDetail/>
+        element: <CountryDetail />,
       },
-    ]
+    ],
   },
-  
-
 ]);
 
 const root = createRoot(document.getElementById("root"));
 
-root.render( <RouterProvider router={router} />);
+root.render(<RouterProvider router={router} />);
